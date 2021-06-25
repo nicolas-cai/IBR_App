e0d80bd43fc6c8925e6994346006846c73afb1b2
Nicolas Cai <nxc172012@gmail.com>
2021-06-25 10:19:47 -0400

    Added version control to android project

--- app/src/main/java/com/example/ibrapp/MainActivity.java
+++ app/src/main/java/com/example/ibrapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -27,6 +28,10 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -36,9 +41,11 @@ import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity  {
     private static int incorrect = 0;
-    //private Spinner spinnerYesNo_1, spinnerYesNo_2, spinnerYesNo_3, spinnerYesNo_4;
     private int currentQuestionIndex = 0;
-    private static String ip = "192.168.1.78";
+    //private static String ip = "192.168.1.78";
+    private static final String url = "jdbc:mysql://192.168.1.78:3306/survey";
+    private static final String user = "root";
+    private static final String pass = "into1812";
     private Connection connection = null;
     private Question[] questionBank = new Question[]{ //CHANGE ANSWER KEY TO INDEX IN SPINNER ARRAY
             new Question(R.string.question_1, 1), //0 = blank, 1 = yes, 2 = no
@@ -46,6 +53,9 @@ public class MainActivity extends AppCompatActivity  {
             new Question(R.string.question_3, 1),
             new Question(R.string.question_4, 2)
     };
+    private int [] responses = new int [questionBank.length];
+
+    TextView question;
     private Spinner [] menus = new Spinner[4];
 
     @Override
@@ -56,15 +66,11 @@ public class MainActivity extends AppCompatActivity  {
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        question = (TextView) findViewById(R.id.textView);
         menus[0] = findViewById(R.id.spinnerYesNo1);
         menus[1] = findViewById(R.id.spinnerYesNo2);
         menus[2] = findViewById(R.id.spinnerYesNo3);
         menus[3] = findViewById(R.id.spinnerYesNo4);
-        /*spinnerYesNo_1 = findViewById(R.id.spinnerYesNo1);
-        spinnerYesNo_2 = findViewById(R.id.spinnerYesNo2);
-        spinnerYesNo_3 = findViewById(R.id.spinnerYesNo3);
-        spinnerYesNo_4 = findViewById(R.id.spinnerYesNo4);*/
 
         String [] responses = getResources().getStringArray(R.array.yes_no);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, responses);
@@ -73,33 +79,13 @@ public class MainActivity extends AppCompatActivity  {
             menu.setAdapter(adapter);
         }
 
-        //String answerChoice1 = spinnerYesNo_1.getSelectedItem().toString();
-        /*spinnerYesNo_1.setOnItemSelectedListener(new OnItemSelectedListener() {
-
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-                if(!answerChoice1.equals(questionBank[0].isAnswerTrue())){
-                    incorrect++;
-                    System.out.println("After: "+ incorrect);
-                }
-                else if(answerChoice1.equals("")){
-                    String error = "This option can't be empty";
-                    setSpinnerError(spinnerYesNo_1, error);
-                    //Toast.makeText(MainActivity.this, error, Toast.LENGTH_LONG).show();
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                //setSpinnerError(spinnerYesNo_1, "Field can't be empty");
-            }
-        });*/
     }
 
     public void buttonPressed(View v){
         EditText id = (EditText) findViewById(R.id.editText);
         EditText name = (EditText) findViewById(R.id.editTextPersonName);
+        Intent intent = new Intent(this, SubmissionActivity.class);
+        //Button btn = findViewById(R.id.button);
         if(TextUtils.isEmpty(id.getText()) && TextUtils.isEmpty(name.getText())) {
             id.setError("Required field");
             id.setTextColor(Color.RED);
@@ -117,11 +103,21 @@ public class MainActivity extends AppCompatActivity  {
         else{
             for(int i = 0; i < questionBank.length; i++){
                 String answerChoice = menus[i].getSelectedItem().toString();
-                if(!(questionBank[i].isAnswerTrue() == Collections.singletonList(R.array.yes_no).indexOf(answerChoice))){
+                int ansId = 0;
+                if(answerChoice.equals("")){
+                    setSpinnerError(menus[i], "Required field"); //display error message for each blank---FIX
+                }
+                else if(answerChoice.equals("Yes")){
+                    ansId=1;
+                }
+                else{
+                    ansId=2;
+                }
+                responses[i] = ansId;
+                if(!(questionBank[i].isAnswerTrue() == responses[i])){
                     incorrect++;
                 }
             }
-            Intent intent = new Intent(this, SubmissionActivity.class);
             startActivity(intent);
         }
     }
@@ -136,4 +132,44 @@ public class MainActivity extends AppCompatActivity  {
     public static int getIncorrect(){
         return incorrect;
     }
+
+    private class ConnectMySql extends AsyncTask<String, Void, String> { //MySQL connection class
+        String res = "";
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(MainActivity.this, "Please wait...", Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection(url, user, pass);
+                System.out.println("Databaseection success");
+
+                String result = "Database Connection Successful\n";
+                Statement st = con.createStatement();
+                ResultSet rs = st.executeQuery("select `question` from `qs`"); //mySQL code
+                ResultSetMetaData rsmd = rs.getMetaData();
+
+                while (rs.next()) {
+                    result += rs.getString(1).toString() + "\n";
+                }
+                res = result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                res = e.toString();
+            }
+            return res;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            question.setText(result);
+        }
+    }
 }
+
